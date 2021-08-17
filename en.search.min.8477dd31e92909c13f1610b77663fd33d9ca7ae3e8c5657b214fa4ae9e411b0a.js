ec6f4bafd6a4037033c59f49ac2d7d0da7fec0c2
nquangtrung <nquangtrung@gmail.com>
2021-08-17 07:16:28 +0000

    Deploying to gh-pages from @ sangsoft/sang.cafe-docs@8252b8d0c68da2b87e83b6360c484032920df260 🚀

--- en.search.min.8477dd31e92909c13f1610b77663fd33d9ca7ae3e8c5657b214fa4ae9e411b0a.js
+++ en.search.min.8477dd31e92909c13f1610b77663fd33d9ca7ae3e8c5657b214fa4ae9e411b0a.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const searchDataURL='/en.search-data.min.870b664c12ac79ccd78c108a7b9d68c1247705dedb90d07dda9ede783276abdb.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+'use strict';(function(){const searchDataURL='/en.search-data.min.fa98e74e32aecba61369c359352358382c41d3342c537f4613a3fafde49185bc.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
 input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
